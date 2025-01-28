703a082296c2ef98385fe236bdc45b3111cfd2fd
RykerInk <rykerink@gmail.com>
2025-01-28 10:37:52 -0800

    Update products.js

--- js/products.js
+++ js/products.js
@@ -28,52 +28,52 @@ export const products = [
     image: 'images/4.jpg',
 },
 {
-    id: '5',
-    image: 'images/5.jpg',
-},
-{
     id: '6',
     image: 'images/6.jpg',
+},
+{
+    id: '25',
+    image: 'images/25.jpg',
 },
 {
     id: '7',
     image: 'images/7.jpg',
 },
 {
-    id: '8',
-    image: 'images/8.jpg',
+    id: '26',
+    image: 'images/26.jpg',
 },
 {
-    id: '9',
-    image: 'images/9.jpg',
+    id: '5',
+    image: 'images/5.jpg',
 },
 {
-    id: '10',
-    image: 'images/10.jpg',
-},
-{
-    id: '11',
-    image: 'images/11.jpg',
+    id: '27',
+    image: 'images/27.jpg',
 },
 {
     id: '12',
     image: 'images/12.jpg',
 },
 {
-    id: '13',
-    image: 'images/13.jpg',
+    id: '28',
+    image: 'images/28.jpg',
+},
+{
+    id: '8',
+    image: 'images/8.jpg',
+},
+{
+    id: '22',
+    image: 'images/22.jpg',
 },
 {
     id: '14',
     image: 'images/14.jpg',
 },
 {
-    id: '15',
-    image: 'images/15.jpg',
-},
-{
-    id: '16',
-    image: 'images/16.jpg',
+    id: '13',
+    image: 'images/13.jpg',
 },
 {
     id: '17',
@@ -96,15 +96,31 @@ export const products = [
     image: 'images/21.jpg',
 },
 {
-    id: '22',
-    image: 'images/22.jpg',
+    id: '15',
+    image: 'images/15.jpg',
 },
 {
-    id: '23',
-    image: 'images/23.jpg',
+    id: '16',
+    image: 'images/16.jpg',
 },
 {
     id: '24',
     image: 'images/24.jpg',
+},
+{
+    id: '21',
+    image: 'images/21.jpg',
+},
+{
+    id: '10',
+    image: 'images/10.jpg',
+},
+{
+    id: '11',
+    image: 'images/11.jpg',
+},
+{
+    id: '23',
+    image: 'images/23.jpg',
 },
 ];
