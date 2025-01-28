63dd4453480552e334fd79418638f8d4e435dd72
RykerInk <rykerink@gmail.com>
2025-01-27 16:04:42 -0800

    Update products.js

--- js/products.js
+++ js/products.js
@@ -13,98 +13,98 @@ export function getProduct(productId) {
 export const products = [
 {
     id: '1',
-    image: 'images/1.jpeg',
+    image: 'images/1.jpg',
 },
 {
     id: '2',
-    image: 'images/2.jpeg',
+    image: 'images/2.jpg',
 },
 {
     id: '3',
-    image: 'images/3.jpeg',
+    image: 'images/3.jpg',
 },
 {
     id: '4',
-    image: 'images/4.jpeg',
+    image: 'images/4.jpg',
 },
 {
     id: '5',
-    image: 'images/5.jpeg',
+    image: 'images/5.jpg',
 },
 {
     id: '6',
-    image: 'images/6.jpeg',
+    image: 'images/6.jpg',
 },
 {
     id: '7',
-    image: 'images/7.jpeg',
+    image: 'images/7.jpg',
 },
 {
     id: '8',
-    image: 'images/8.jpeg',
+    image: 'images/8.jpg',
 },
 {
     id: '9',
-    image: 'images/9.jpeg',
+    image: 'images/9.jpg',
 },
 {
     id: '10',
-    image: 'images/10.jpeg',
+    image: 'images/10.jpg',
 },
 {
     id: '11',
-    image: 'images/11.jpeg',
+    image: 'images/11.jpg',
 },
 {
     id: '12',
-    image: 'images/12.jpeg',
+    image: 'images/12.jpg',
 },
 {
     id: '13',
-    image: 'images/13.jpeg',
+    image: 'images/13.jpg',
 },
 {
     id: '14',
-    image: 'images/14.jpeg',
+    image: 'images/14.jpg',
 },
 {
     id: '15',
-    image: 'images/15.jpeg',
+    image: 'images/15.jpg',
 },
 {
     id: '16',
-    image: 'images/16.jpeg',
+    image: 'images/16.jpg',
 },
 {
     id: '17',
-    image: 'images/17.jpeg',
+    image: 'images/17.jpg',
 },
 {
     id: '18',
-    image: 'images/18.jpeg',
+    image: 'images/18.jpg',
 },
 {
     id: '19',
-    image: 'images/19.jpeg',
+    image: 'images/19.jpg',
 },
 {
     id: '20',
-    image: 'images/20.jpeg',
+    image: 'images/20.jpg',
 },
 {
     id: '21',
-    image: 'images/21.jpeg',
+    image: 'images/21.jpg',
 },
 {
     id: '22',
-    image: 'images/22.jpeg',
+    image: 'images/22.jpg',
 },
 {
     id: '23',
-    image: 'images/23.jpeg',
+    image: 'images/23.jpg',
 },
 {
     id: '24',
-    image: 'images/24.jpeg',
+    image: 'images/24.jpg',
 },
 ];
